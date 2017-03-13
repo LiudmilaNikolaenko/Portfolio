21ddc3126b42155035918968d597dfd42fc0bae2
Liudmila Nikolaenko <lldubinina@mail.ru>
2017-03-13 11:45:10 +0200

    new files

--- BuildHome/gruntfile.js
+++ BuildHome/gruntfile.js
@@ -1,28 +1,67 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     sass: {
       dist: {
         files: [{
           expand: true,
-          cwd: 'css/',
+          cwd: 'src/css/',
           src: ['*.scss'],
-          dest: 'css/',
+          dest: 'src/css/',
           ext: '.css'
         }]
       }
     },
     watch: {
       css: {
-        files: 'css/*.scss',
+        files: 'src/css/*.scss',
         tasks: ['sass'],
       },
+    },
+    concat: {
+      js: {
+        src: ['src/js/modernizr-custom.js','src/js/jcarousel.transitions.js'],
+        dest: 'src/js/main.js'
+      },
+      css: {
+        src: ['src/css/reset.css','src/css/icomoonstyle.css', 'src/css/jcarousel.transitions.css', 'src/css/style.css'],
+        dest: 'src/css/main.css'
+      }
+    },
+    uglify: {
+      js: {
+        files: {
+          'js/main.min.js': ['src/js/main.js']
+        }
+      }
+    },
+    cssmin: {
+      css: {
+        files: {
+          'css/main.min.css': ['src/css/main.css']
+         }
+      }
+    },
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/img/',
+          src: ['*.{png,jpg}'],
+          dest: 'img/'
+        }]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'imagemin']);
 
 };
--- Urlaubsgluck/gulpfile.js
+++ Urlaubsgluck/gulpfile.js
@@ -13,45 +13,52 @@ var cache = require('gulp-cache');
 var imagemin = require('gulp-imagemin');
 
 gulp.task('sass', function () {
-	return gulp.src('scss/*.scss')
+	return gulp.src('src/scss/*.scss')
 		.pipe(sass().on('error', sass.logError))
-    	.pipe(gulp.dest('css/'));
+    	.pipe(gulp.dest('src/css/'));
 });
  
 gulp.task('watch', function () {
-	gulp.watch('scss/*.scss', ['sass']);
+	gulp.watch('src/scss/*.scss', ['sass']);
 });
 
 gulp.task('sprite', function () {
-  var spriteData = gulp.src('imgpng/*.png').pipe(spritesmith({
+  var spriteData = gulp.src('src/imgpng/*.png').pipe(spritesmith({
     imgName: 'sprite.png',
     cssName: 'sprite.css',
     padding: 2
   }));
-  return spriteData.pipe(gulp.dest('img/'));
+  return spriteData.pipe(gulp.dest('src/img/'));
 });
 
 gulp.task('sprite@2x', function () {
-  var spriteData = gulp.src('imgpng@2x/*.png').pipe(spritesmith({
+  var spriteData = gulp.src('src/imgpng@2x/*.png').pipe(spritesmith({
     imgName: 'sprite@2x.png',
     cssName: 'sprite@2x.css',
     padding: 4
   }));
-  return spriteData.pipe(gulp.dest('img/'));
+  return spriteData.pipe(gulp.dest('src/img/'));
 });
 
 gulp.task('useref', function(){
-	return gulp.src('index.html')
+	return gulp.src('src/index.html')
 		.pipe(gulpIf('*.css', cleanCSS()))
 		.pipe(gulpIf('*.js', uglify()))
 		.pipe(useref())
-		.pipe(gulp.dest('dist'))
+		.pipe(gulp.dest('./'))
 });
 
 gulp.task('img', function(){
-    return gulp.src('img/*.+(png|jpg|jpeg|gif|svg)')
+    return gulp.src('src/img/*.+(png|jpg|jpeg|gif|svg)')
         .pipe(cache(imagemin()))
-        .pipe(gulp.dest('dist/img/'))
+        .pipe(gulp.dest('./img/'))
 });
+
+gulp.task('fonts', function(){
+    return gulp.src('src/fonts/*.*')
+        .pipe(gulp.dest('./fonts/'))
+});
+
+gulp.task('build', ['useref', 'img', 'fonts']);
 
 gulp.task('default', ['sass', 'watch']);
